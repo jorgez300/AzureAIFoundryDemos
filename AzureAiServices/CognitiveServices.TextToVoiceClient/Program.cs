13aeca90532a33cd0ac2c5b7998d3a11362526a2
JorgeGonzalez <ing.jorge.gonzalez92@gmail.com>
2025-09-30 21:41:37 -0400

    Added CognitiveServices.TextToVoiceClient project

---
+++ AzureAiServices/CognitiveServices.TextToVoiceClient/Program.cs
@@ -0,0 +1,13 @@
+﻿using CognitiveServices.TextToVoiceClient;
+using Microsoft.Extensions.Configuration;
+
+// Leer configuración de appsettings.json
+var config = new ConfigurationBuilder()
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+
+var _client = new TTSClient(config);
+
+await _client.GenerateSpeechAsync("Hola, este es un ejemplo de texto a voz usando Azure OpenAI y Cognitive Services", "alloy");
---
+++ AzureAiServices/CognitiveServices.TextToVoiceClient/TTSClient.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CognitiveServices.TextToVoiceClient
+{
+    public class TTSClient
+    {
+        IConfigurationRoot _config;
+        public TTSClient(IConfigurationRoot config)
+        {
+            this._config = config;
+        }
+
+        public async Task GenerateSpeechAsync(string inputText, string voice = "alloy")
+        {
+            var url = this._config["AzureOpenAI:Endpoint"];
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", this._config["AzureOpenAI:ApiKey"]);
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            var payload = new
+            {
+                model = this._config["AzureOpenAI:DeploymentName"],
+                input = inputText,
+                voice = voice
+            };
+            var json = JsonSerializer.Serialize(payload);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await client.PostAsync(url, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var audioBytes = await response.Content.ReadAsByteArrayAsync();
+                var fileName = $"Audio{DateTime.Now:HHmmss}.wav";
+                await File.WriteAllBytesAsync(fileName, audioBytes);
+                Console.WriteLine($"Audio guardado en {fileName}");
+            }
+            else
+            {
+                var error = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"Error: {response.StatusCode}\n{error}");
+            }
+        }
+    }
+}
